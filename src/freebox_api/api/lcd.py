93ac942fac7ed2a8fab25cc9ff1f95dd73a4e31c
Bruce LIOU <73282121+BruceLIOU@users.noreply.github.com>
2024-12-17 20:36:07 +0100

    LCD: add led_strip lcd_config_schema (Freebox Ultra Limited Edition) (#746)
    Co-authored-by: Bruce LIOU <b.liou@weekera.fr>
    Co-authored-by: Quentame <polletquentin74@me.com>

--- src/freebox_api/api/lcd.py
+++ src/freebox_api/api/lcd.py
@@ -18,6 +18,17 @@ class Lcd:
         "orientation": 0,
         "brightness": 100,
         "orientation_forced": False,
+        "available_led_strip_animations": [
+            "organic",
+            "static",
+            "breathing",
+            "rain",
+            "trail",
+            "wave",
+        ],
+        "led_strip_brightness": 56,
+        "led_strip_animation": "breathing",
+        "led_strip_enabled": True,
     }
 
     async def get_configuration(self):
